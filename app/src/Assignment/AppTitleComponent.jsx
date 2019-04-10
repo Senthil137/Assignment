2d4b51b151d9e1028625b80082af90978515bc16
Senthil137 <senthilalagar03@gmail.com>
2019-04-10 09:13:46 +0530

    Created header section structure

---
+++ app/src/Assignment/AppTitleComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+//import Box from '@material-ui/core/Box';
+
+
+const AppTitle = (props) => {
+    return (
+        <h1> {props.test}</h1>
+    )
+}
+
+export default AppTitle;
